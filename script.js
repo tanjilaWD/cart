8cc19b9a296e579f332fbd41e04d9b74e326bf16
Tanjila Khatun <tanjilabithi12@gmail.com>
2024-06-26 21:45:51 +0600

    new

---
+++ script.js
@@ -0,0 +1,30 @@
+const product =[
+    {
+    id: 0,
+    image: 'download-3.png',
+    title: 'z Flip Foldable Mobile',
+    price: 120,
+},
+
+{
+    id: 1,
+    image: 'download_2.png',
+    title: 'Air Pods Pro',
+    price: 60,
+},
+
+{
+    id: 2,
+    image: 'download_1.png',
+    title: '250D DSLR Camera',
+    price: 230,
+},
+
+{
+    id: 3,
+    image: 'download_4.png',
+    title: 'Head Phone',
+    price: 100,
+}
+
+];
