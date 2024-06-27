cb43ac68b3dfc967c89677b7b45b36809529444c
Tanjila Khatun <tanjilabithi12@gmail.com>
2024-06-27 10:13:27 +0600

    new

--- script.js
+++ script.js
@@ -49,3 +49,27 @@ document.getElementById('root').innerHTML = categories.map((item)=>
 
     )
 }).join('')
+
+var cart =[];
+function displaycart(a){
+    let j = 0;
+    if(cart.length==0){
+        document.getElementById('cartItem').innerHTML = "Your cart is empty";
+    }
+    else{
+        document.getElementById('cartItem').innerHTML = cart.map((items)=>
+            {
+                var {image, title, price}= items;
+                return(
+                    `<div class='cart-item'>
+                    <div class ='rowimg' src=${image}>
+                    </div>
+                    <p style ='font-size:12px;'>${title}</p> 
+                    <h2 style = 'font-size:15px;'>$ ${price}.00</h2>`+
+                    "<i class = 'fas fa-trash' onclinc='delElement ("+(j++)+") "
+                )
+
+        })
+        
+    }
+}
